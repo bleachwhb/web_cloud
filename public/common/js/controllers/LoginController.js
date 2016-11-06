5db4ebf69bdf78d6d5ad5d6d8714e0eef4b96007
Dicky Shi <DickyShi@DickyShi.local>
2016-11-06 16:25:35 -0600

    remove code

--- public/common/js/controllers/LoginController.js
+++ public/common/js/controllers/LoginController.js
@@ -31,37 +31,6 @@ angular.module('app')
             console.log(error);
             alert('Invalid username/password combination' + error.code + ' ' + error.message);
           });
-        // User.logIn($scope.email, $scope.password, {
-        //   success: function (user) {
-        //     if (user) {
-        //       var pp = user.get('patientPointer');
-        //       var dp = user.get('doctorPointer');
-        //       if (($scope.accType == 'Patient') && pp) {
-        //         console.log('patient');
-        //         $scope.$apply(function () {
-        //           $location.path('/patient/profile').replace();
-        //           $rootScope.currentUser = user;
-        //         });
-        //       }
-        //       else if (($scope.accType == 'Doctor') && dp) {
-        //         console.log('doctor');
-        //         $scope.$apply(function () {
-        //           $location.path('/patient/profile').replace();
-        //           $rootScope.currentUser = user;
-        //         });
-        //       }
-        //       else
-        //         alert("You're attempting to log in as someone you are not");
-        //     }
-        //     else {
-        //       var sUp = confirm('This email was not found in our database. Would you like to sign up?');
-        //       if (sUp === true) UI.redirect('/#/signup');
-        //     }
-        //   },
-        //   error: function (user, error) {
-        //     alert('Invalid username/password combination' + error.code + ' ' + error.message);
-        //   }
-        // });
       };
 
 
